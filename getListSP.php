18c5b8b4acbd113195cf313bdcb3db52d7783a37
hiep <hiep68965@huce.edu.vn>
2023-11-16 20:50:30 +0700

    Hiep commit file getListSP

---
+++ getListSP.php
@@ -0,0 +1,31 @@
+<?php
+    require("connect.php");
+    $mangsp = array();
+    
+    $query = "select * from SanPham limit 10";
+    $data = $conn->query($query) or die($conn->error);
+    if($data->num_rows == 0){
+        echo "Khong co du lieu";
+    }
+    else{
+        while($row = $data->fetch_assoc()){
+            array_push($mangsp, new SanPham(
+                $row["SP_MaSP"],
+                $row["SP_TenSP"],
+                $row["SP_GiaSP"],
+                $row["SP_AnhSP"],
+                $row["SP_SoLuong"]));
+        }
+        echo json_encode($mangsp);
+    }
+    
+    class SanPham{
+        function __construct($pid, $tensp, $giasp, $anhsp, $soluong){
+            $this->id = $pid;
+            $this->tensp = $tensp;
+            $this->giasp = $giasp;
+            $this->anhsp = $anhsp;
+            $this->soluong = $soluong;
+        }
+    }
+?>
